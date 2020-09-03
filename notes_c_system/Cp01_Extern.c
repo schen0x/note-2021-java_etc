367b1f24c3dc9752a624238acfc52301e876962c
chnnn <chnnn.git@gmail.com>
2020-09-03 22:15:30 +0000

    c extern

---
+++ notes_c_system/Cp01_Extern.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define Cp01_A 2000
+const int Cp01_i = 1;
+long Cp01_j = 1L;
+
+int main(){
+    printf("%d \n", Cp01_A);
+    printf("%ld \n",  Cp01_i);
+    printf("%d ",  Cp01_j);
+    return 0;
+}
