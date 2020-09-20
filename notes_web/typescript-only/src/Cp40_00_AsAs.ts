d7b42d2bebe19db99b60fbab0a5c3e49011cb7f5
chnnn <chnnn.git@gmail.com>
2020-09-20 22:36:45 +0000

    as as

--- notes_web/typescript-only/src/Cp40_00_AsAs.ts
+++ notes_web/typescript-only/src/Cp40_00_AsAs.ts
@@ -1,1 +1,17 @@
-let t 
+interface proto {
+    name: string;
+    action:()=>void;
+}
+
+export class potato implements proto{
+    name="potato"
+    action = function a(){
+        return;
+    }
+}
+let t = {} as any as proto;
+t.action; // prompted
+
+let t1 = {} as any as unknown as proto;
+
+let t2 = {} as proto as any as number;
