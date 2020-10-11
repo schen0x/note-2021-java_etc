dc167c360d5dcfc3f5d74ef02e019b27273fc769
chnnn <chnnn.git@gmail.com>
2020-10-11 23:50:43 +0000

    books & bitfields

--- notes_c_system/Cp05_BitFields.c
+++ notes_c_system/Cp05_BitFields.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <ctype.h>
-struct 
+struct s
 {
-    unsigned short icon : 8;
-    unsigned short color : 4;
-    unsigned short underline : 1;
+    unsigned short icon : 4;
+    unsigned short color : 2;
     unsigned short blink : 1;
-} someBitFields;
+} someBitFields; // to save space
+// }__attribute__((packed)) someBitFields; // packed attribute, compiler will ignore alignment, code slower, but more packed.
 
+#define NUM_LOOPS 20
 int main(int argc, char const *argv[])
 {
-    // TODO
+    for (int i = 0; i < NUM_LOOPS; i++)
+    {
+        someBitFields.color++;
+        someBitFields.icon++;
+        someBitFields.blink++;
+        printf("%u, %u, %u, %u \n", someBitFields.icon, someBitFields.color, someBitFields.blink, someBitFields);
+    }
     printf("%d\n", sizeof(someBitFields));
+    printf("%d\n", sizeof(struct s));
     printf("------------\n");
     return 0;
 }
