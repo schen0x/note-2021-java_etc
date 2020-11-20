62ff9b5d19e433fa4468214500fe49f70bc2a7eb
chnnn <chnnn.git@gmail.com>
2020-11-20 18:48:36 +0900

    OSINT basic

--- notes_gsec/ut_dumpSubdomainFromFile.py
+++ notes_gsec/ut_dumpSubdomainFromFile.py
@@ -1,6 +1,7 @@
 # 1. extract all subdomain name. r'(?<=\/\/)((\w\.)+(\w)+)'
 import re
 import sys
+from io import TextIOWrapper
 ptn = re.compile(r'(?<=\/\/)(([\w0-9-]*\.)+(\w)+)')
 
 tgt = '''
@@ -12,10 +13,12 @@ http://www.test3.org
 '''
 
 # FIXME if(stdin)
-# if (sys.stdin):
-    # tgt = sys.stdin
+# sysStdIn: TextIOWrapper = sys.stdin
+dtIn = sys.stdin.read().encode('utf-8')
+if (len(dtIn) > 0):
+    tgt2 = dtIn
+    print(tgt2)
 
 rsraw = ptn.findall(tgt)
 rs = set([r[0] for r in rsraw ])
-
 print(*rs, sep='\n')
