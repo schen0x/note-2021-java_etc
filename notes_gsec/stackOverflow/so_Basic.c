a50a1fa7967cf701ddb5abce6f429d0371ee2b3c
chnnn <chnnn.git@gmail.com>
2020-11-22 17:49:30 +0000

    gdb basic

--- notes_gsec/stackOverflow/so_Basic.c
+++ notes_gsec/stackOverflow/so_Basic.c
@@ -6,8 +6,10 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         printf("Error - You must supply at least one argument\n");
+        return 1;
     }
-    return 1;
+
     strcpy(buffer, argv[1]);
-return 0;
+
+    return 0;
 }
