498c260cfe9d53942db3956ae50b922470a7decd
schen0x <schen.git@gmail.com>
2021-06-02 23:44:08 +0900

    py regex misc

--- notes_0ds/practices/ut_regex.py
+++ notes_0ds/practices/ut_regex.py
@@ -7,12 +7,15 @@ AEBCD_EFG
 '''
 
 # simple match
-print('----------start-----------')
+print('\n=============start===============')
 res = re.match("a\w+$", "abc")
 print(res)
 print(res.groups())
 print('------')
-
+# search vs match, match only check from the start.
+print(re.match("b\w+$", "abc"))
+print(re.search("b\w+$", "abc"))
+print('------')
 # with re.compile
 pattern = re.compile(r'(a|b)[cC](d)', re.IGNORECASE)
 matches = pattern.finditer(text_to_search)
@@ -22,10 +25,7 @@ for match in matches:
     print(match.group(0)) # 0 for all.
 
 print('------')
-
 print(pattern.sub(r'\1 | \2', text_to_search))
 matches = pattern.finditer(text_to_search)
 print(pattern.findall(text_to_search))
-
 print('abc'.__class__)
-print('----------end-----------')
