bdf72b14dac921ae792744232fb86d9b176521c2
schen0x <schen.git@gmail.com>
2021-07-30 20:48:41 +0900

    dump subdomain

--- notes_0ds/ut_DumpSubdomainFromFile.py
+++ notes_0ds/ut_DumpSubdomainFromFile.py
@@ -1,16 +1,23 @@
-# 1. extract all subdomain name. r'(?<=\/\/)((\w\.)+(\w)+)'
+# extract all subdomain name from an [html] file.
+# Usage: python3 ./ut_DempSubdomainFromFile.py ./index.html
+
 import re
 
 tgt = "index.html"
 pt = re.compile(
     r'(?<=\/\/)(([-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b)([-a-zA-Z0-9()@:%_\+.~#?&/=]*))')
-######## example matches ##########
+
+# example matches
+# --------------------------------------------------------------------------------
 # play.google.com/?hl=ja&tab=w8
 # schema.org/WebPage
 # www.google.com/url?q=https://store.google.com/product/chromecast_google_tv%3Futm_source%3Dhpp%26utm_medium%3Dgoogle_oo%26utm_campaign%3DGS105832&amp
 
-outList = []
-# output group[1] only, e.g. play.google.com
+# other regex options: r'(?<=\/\/)((\w\.)+(\w)+)'
+
+# group 1 contains host part only. e.g. play.google.com
+regex_match_g00 = []
+regex_match_g01 = []
 with open(tgt, "rb") as f:
     ct = f.read()
     ct = ct.decode('utf-8')
@@ -18,41 +25,22 @@ with open(tgt, "rb") as f:
     matches = pt.findall(ct)
     matches = sorted(matches, key=lambda x: x[1])
     seen = set()
-    results = [seen.add(m[1]) or m[1] for m in matches if m[1] not in seen]
-    for m in results:
+
+    res_g00 = [seen.add(m[0]) or m[0] for m in matches if m[0] not in seen]
+    res_g01 = [seen.add(m[1]) or m[1] for m in matches if m[1] not in seen]
+    for m in res_g00:
         print(m)
-        outList.append(m)
+        regex_match_g00.append(m)
+    print('---------------------------------\n')
+    for m in res_g01:
+        # print(m)
+        regex_match_g01.append(m)
 
 # write hosts to file for further inspection
 outFileName = tgt.partition('.')[0] + '_hosts'
 with open(outFileName, 'w') as f:
-    for m in outList:
+    for m in regex_match_g01:
         f.write(m + '\n')
 
 # then loop through the file in the shell:
 # while read p; do host $p >> index_hosts_result; done < index_hosts
-
-print('---------------------------------\n')
-# print group[0]
-with open(tgt, "rb") as f:
-    ct = f.read()
-    ct = ct.decode('utf-8')
-    # matches = pt.finditer(ct)
-    matches = pt.findall(ct)
-    matches = sorted(matches, key=lambda x: x[1])
-    seen = set()
-    results = [seen.add(m[0]) or m[0] for m in matches if m[0] not in seen]
-    for m in results:
-        print(m)
-
-
-
-###############################################
-# extra debugging, grep a line contains match.
-pt = re.compile(r'(?<=(\/\/))(\w*\.?\w*)+')
-with open(tgt, "rb") as fl:
-    lines = fl.readlines()
-    print(len(lines))
-    for line in lines:
-        if(pt.search(ct)):
-            print(line)
